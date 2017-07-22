882e333fc848e0daf7d061f3649ad3f2c6ad76e1
sayan <moitrasayan007@gmail.com>
2017-07-22 10:37:30 +0000

    Added the postgresql cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -7,9 +7,9 @@
 #
 #
 
-if node ['platform_family']=="rhel"
+if node['platform_family'] == "rhel"
 	package = "httpd"
-elsif node ['platform_family']=="debian"
+elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
 
